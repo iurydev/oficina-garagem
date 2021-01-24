3a0bd491f6d667fa97d83e5f6e498e0b3de5ac05
Iury Rodrigues <iurydev@MacBook-Pro-Iury-Rodrigues.local>
2021-01-24 10:01:12 -0300

    Modificacoes módulo de servicos

--- src/modules/administrativo/home-administrativo/servicos-adm/servicos-adm.component.ts
+++ src/modules/administrativo/home-administrativo/servicos-adm/servicos-adm.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-servicos-adm',
-  templateUrl: './servicos-adm.component.html',
-  styleUrls: ['./servicos-adm.component.css']
+  selector: "app-servicos-adm",
+  templateUrl: "./servicos-adm.component.html",
+  styleUrls: ["./servicos-adm.component.css"],
 })
 export class ServicosAdmComponent implements OnInit {
+  formulario: FormGroup;
+  exibeAddServico: boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.IniciarFormulario();
   }
 
+  IniciarFormulario() {
+    this.formulario = this.formBuilder.group({
+      control_descricao: [null, Validators.required],
+      control_cliente: [null, Validators.required],
+      control_valor: [null, Validators.required],
+      control_data_entrega: [null, Validators.required],
+      control_status: [null, Validators.required],
+    });
+  }
+
+  AddNovoServico() {
+    this.exibeAddServico = true;
+  }
 }
